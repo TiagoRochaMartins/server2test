79e9790bc574466ef15713e06c442c9e457758e0
TiagoRochaMartins <114081863+TiagoRochaMartins@users.noreply.github.com>
2023-05-24 15:32:20 +0000

    Mais um dia 24_05_2023

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.listen(8000, () => {
   console.log('Servidor a funcionar na porta 8000');
 });*/
 
-const http = require('http');
+/*const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
@@ -36,4 +36,34 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, () => {
   console.log('Servidor a funcionar na porta 8000');
+});*/
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // Lê o arquivo index.html e envia para o cliente
+    fs.readFile('ficheiro1.html', (err, data) => { 
+      if (err) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Erro interno do servidor');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        // Adiciona o link para a página 2 no GitHub
+        const content = data.toString().replace('<a href="ficheiro2.html">Abrir Página 2</a>', '<a href="ficheiro2.html">Abrir Página 2</a>');
+        res.end(content);
+      }
+    });
+  } else {
+    // URL inválido - Retorna um erro 404
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Página não encontrada');
+  }
 });
+
+server.listen(8000, () => {
+  console.log('Servidor a funcionar na porta 8000');
+});
+
+
