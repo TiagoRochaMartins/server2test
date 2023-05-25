926db4ff1acd39af9441ee28080fe2e8760720ed
TiagoRochaMartins <114081863+TiagoRochaMartins@users.noreply.github.com>
2023-05-25 10:41:37 +0000

    Mais um commit de hoje

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ server.listen(8000, () => {
   console.log('Servidor a funcionar na porta 8000');
 });*/
 
-const http = require('http');
+/*const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
@@ -64,7 +64,38 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, () => {
   console.log('Servidor a funcionar na porta 8000');
+});*/
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './ficheiro1.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Página não encontrada');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (filePath === './ficheiro1.html') {
+        data = data.toString()
+          .replace('<a href="ficheiro2.html">Abrir Página 2</a>', '<a href="/ficheiro2.html">Abrir Página 2</a>')
+          .replace('<a href="ficheiro3.html">Abrir Página 3</a>', '<a href="/ficheiro3.html">Abrir Página 3</a>');
+      }
+      res.end(data);
+    }
+  });
 });
+
+server.listen(8000, () => {
+  console.log('Servidor a funcionar na porta 8000');
+});
+
 
 
 
