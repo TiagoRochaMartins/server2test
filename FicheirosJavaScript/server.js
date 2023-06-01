7a595d6091c451f7054fb2959bedb8f655c9a8ca
TiagoRochaMartins <tiagorochamartins19@gmail.com>
2023-06-01 09:30:48 +0100

    Primeiro do dia 01_06_2023

--- FicheirosJavaScript/server.js
+++ FicheirosJavaScript/server.js
@@ -66,37 +66,6 @@ server.listen(8000, () => {
   console.log('Servidor a funcionar na porta 8000');
 });*/
 
-/*const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath === './') {
-    filePath = './ficheiro1.html';
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Página não encontrada');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      if (filePath === './ficheiro1.html') {
-        data = data.toString()
-          .replace('<a href="ficheiro2.html">Abrir Página 2</a>', '<a href="/ficheiro2.html">Abrir Página 2</a>')
-          .replace('<a href="ficheiro3.html">Abrir Página 3</a>', '<a href="/ficheiro3.html">Abrir Página 3</a>')
-          .replace('<a href="ficheiro4.html">Abrir PáginaQRCode</a>', '<a href="/ficheiro4.html">Abrir PáginaQRCode</a>');
-          
-      }
-      res.end(data);
-    }
-  });
-});
-
-server.listen(8000, () => {
-  console.log('Servidor em execução na porta 8000');
-});*/
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -118,7 +87,7 @@ const server = http.createServer((req, res) => {
           .replace('<a href="ficheiro2.html">Abrir Página 2</a>', '<a href="/FicheirosHTMLWEB/ficheiro2.html">Abrir Página 2</a>')
           .replace('<a href="ficheiro3.html">Abrir Página 3</a>', '<a href="/FicheirosHTMLWEB/ficheiro3.html">Abrir Página 3</a>')
           .replace('<a href="ficheiro4.html">Abrir PáginaQRCode</a>', '<a href="/FicheirosHTMLWEB/ficheiro4.html">Abrir Página 4</a>')
-          .replace('<a href="contactos.html">Abrir PáginaQRCode</a>', '<a href="/FicheirosHTMLWEB/contactos.html">Contactos</a>');
+          .replace('<a href="contactos.html">Abrir PáginaContactos</a>', '<a href="/FicheirosHTMLWEB/contactos.html">Contactos</a>');
       }
       res.end(data);
     }
